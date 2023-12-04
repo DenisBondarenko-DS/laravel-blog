9368c31ab87d2ca28b142374d459d1fd8d38aca4
Denis Bondarenko <fvscorpion2@gmail.com>
2023-12-04 13:37:03 +0200

    Make minor adjustments to the admin panel layout

--- resources/views/admin/layouts/layout.blade.php
+++ resources/views/admin/layouts/layout.blade.php
@@ -58,10 +58,10 @@
             <!-- Sidebar user (optional) -->
             <div class="user-panel mt-3 pb-3 mb-3 d-flex">
                 <div class="image">
-                    <img src="{{ asset('no-user-image.jpg') }}" class="img-circle elevation-2" alt="User Image">
+                    <img src="{{ auth()->user()->getImage() }}" class="img-circle elevation-2" alt="User Image">
                 </div>
                 <div class="info">
-                    <a href="#" class="d-block">{{ auth()->user()->name }}</a>
+                    <a href="{{ route('profile.show') }}" class="d-block">{{ auth()->user()->name }}</a>
                 </div>
             </div>
 
