4c0657ec663a99064d50658f5927a1efc87868f1
Denis Bondarenko <fvscorpion2@gmail.com>
2023-12-05 19:45:14 +0200

    Remove unnecessary code from login method in AuthController

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -36,11 +36,6 @@ class AuthController extends Controller
 
     public function login(LoginRequest $request)
     {
-        $request->validate([
-            'email' => 'required|email',
-            'password' => 'required'
-        ]);
-
         if (Auth::attempt([
             'email' => $request->email,
             'password' => $request->password
